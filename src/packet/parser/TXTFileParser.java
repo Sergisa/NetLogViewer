9c913ffec8910dce656797f557cbcae613ff4c0b
Sergisa <phebra@yandex.ru>
2022-03-22 22:38:18 +0300

    Tests update

--- src/packet/parser/TXTFileParser.java
+++ src/packet/parser/TXTFileParser.java
@@ -105,7 +105,8 @@ public class TXTFileParser extends AbstractParser implements Parser {
 
     public InetAddress resolveAddress(String address) throws UnknownHostException {
         final Pattern IPv4Pattern = Pattern.compile("^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$");
-        final Pattern IPv4SubPattern = Pattern.compile("(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})");
+        final Pattern canonicalNamePattern = Pattern.compile("^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$");
+        final Pattern IPv4SubPattern = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
         final Pattern IPv6Pattern = Pattern.compile("(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]+|::(ffff(:0{1,4})?:)?((25[0-5]|(2[0-4]|1?[0-9])?[0-9])\\.){3}(25[0-5]|(2[0-4]|1?[0-9])?[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1?[0-9])?[0-9])\\.){3}(25[0-5]|(2[0-4]|1?[0-9])?[0-9]))");
         Matcher IPv6Matcher = IPv6Pattern.matcher(address);
         Matcher IPv4Matcher = IPv4Pattern.matcher(address);
--- src/test/TXTFileParserTest.java
+++ src/test/TXTFileParserTest.java
@@ -27,41 +27,21 @@ public class TXTFileParserTest {
     }
 
     @Test
-    public void getPackets() {
+    public void inetAddressConstruction() {
         String yandexIP = "185.9.147.1";
         String yandexDNS = "shared-25.smartape.ru";
         try {
-            //Assertions.assertEquals("126.0.0.1", Inet4Address.getByName(yandexIP).getHostName(), "IP Address to domain");
-            Assertions.assertEquals(yandexDNS, Inet4Address.getByName(yandexIP).getHostName(), "IP Address to domain");
-            Assertions.assertEquals(yandexDNS, Inet4Address.getByName(yandexDNS).getCanonicalHostName(), "Domain to domain");
+            Assertions.assertEquals(yandexIP, Inet4Address.getByName(yandexIP).getHostAddress(), "IP Address was not converted to IP address");
+            Assertions.assertEquals(yandexDNS, Inet4Address.getByName(yandexDNS).getHostAddress(), "Domain was not converted to right Domain name");
+            //Assertions.assertEquals(yandexDNS, Inet4Address.getByName(yandexIP).getHostAddress(), "IP Address was not converted to right Domain name");
         } catch (UnknownHostException e) {
+            Assertions.fail();
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    void sbSrTest() {
-        StringReader sr = new StringReader("""
-                Tue Aug 21 12:27:20 2018; ******** IP traffic monitor started ********\r
-                Tue Aug 21 12:32:26 2018; UDP; eth0; 139 bytes; from 192.168.103.1:45323 to 255.255.255.255:5678\r
-                Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to 192.168.103.253:ftp; first packet\r
-                """);
-        BufferedReader br = new BufferedReader(sr);
-
-        try {
-            Assertions.assertEquals("Tue Aug 21 12:27:20 2018; ******** IP traffic monitor started ********", br.readLine());
-            Assertions.assertEquals("Tue Aug 21 12:32:26 2018; UDP; eth0; 139 bytes; from 192.168.103.1:45323 to 255.255.255.255:5678", br.readLine());
-            Assertions.assertEquals("Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to 192.168.103.253:ftp; first packet", br.readLine());
-            Assertions.assertNull(br.readLine());
-            Assertions.assertNull(br.readLine());
-            Assertions.assertNull(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    void startParse() {
+    void parseSomeLines() {
         TXTFileParser parser = new TXTFileParser();
 
         String packetStrings = """
@@ -70,30 +50,9 @@ public class TXTFileParserTest {
                 Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to 192.168.103.253:ftp; first packet\r
                 """;
         List<Packet> packetList = parser.readStrings(packetStrings);
-
         Assertions.assertEquals(2, packetList.size(), "Illegal row added to packet list");
 
-        Packet firstPacket = Packet.Builder.aPacket()
-                .withDate("Tue Aug 21 12:32:26 2018")
-                .withType(Packet.Type.UDP)
-                .withBytes(139)
-                .withSource("192.168.103.1")
-                .withDestination("255.255.255.255")
-                .build();
-        Packet secondPacket = Packet.Builder.aPacket()
-                .withDate("Tue Aug 21 12:27:43 2018")
-                .withType(Packet.Type.TCP)
-                .withBytes(72)
-                .withSource("81-1-183-199.broadband.progtech.ru")
-                .withDestination("192.168.103.253")
-                .build();
-        packetStrings = """
-                Tue Aug 21 12:27:20 2018; ******** IP traffic monitor started ********\r
-                Tue Aug 21 12:32:26 2018; UDP; eth0; 139 bytes; from 192.168.103.1:45323 to 255.255.255.255:5678\r
-                Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to 192.168.103.253:ftp; first packet\r
-                """;
-        Assertions.assertEquals(firstPacket, parser.readStrings(packetStrings).get(0), "First parsed packet mismatch");
-        Assertions.assertEquals(secondPacket, parser.readStrings(packetStrings).get(1), "Second parsed packet mismatch");
+
     }
 
     @Test
@@ -107,24 +66,34 @@ public class TXTFileParserTest {
         Assertions.assertThrows(AbstractParser.FileParserException.class, () -> parser.parseStep(illegalDatePacket), "Illegal date parse doesn't thrown");
 
         String testPacket = "Tue Aug 21 12:27:26 2018; UDP; eth0; 139 bytes; from 192.168.103.1 to 255.255.255.255";
-        Packet packet = Packet.Builder.aPacket()
+        Packet IPtoIP_Packet = Packet.Builder.aPacket()
                 .withDate("Tue Aug 21 12:27:26 2018")
                 .withType(Packet.Type.UDP)
                 .withBytes(139)
                 .withSource("192.168.103.1")
                 .withDestination("255.255.255.255")
                 .build();
-        Assertions.assertEquals(packet, parser.parseStep(testPacket), "Not recognized packet data");
-    }
+        Assertions.assertEquals(IPtoIP_Packet, parser.parseStep(testPacket), "Not recognized packet data");
 
-    @Test
-    void replacementTest() {
-        /*try {
-            System.out.println(InetAddress.getByName( "127.0.0.1:3306".replaceAll(":[\\d\\w]+", "")));
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }*/
-        Assertions.assertEquals("127.0.0.1", "127.0.0.1:3306".replaceAll(":[\\d\\w]+", ""));
-        Assertions.assertEquals("127.0.0.1", "127.0.0.1".replaceAll(":[\\d\\w]+", ""));
+        Packet DNStoDNS_Packet = Packet.Builder.aPacket()
+                .withDate("Tue Aug 21 12:27:43 2018")
+                .withType(Packet.Type.TCP)
+                .withBytes(72)
+                .withSource("81-1-183-199.broadband.progtech.ru")
+                .withDestination("www.yandex.ru")
+                .build();
+        Packet IPtoDNS_Packet = Packet.Builder.aPacket()
+                .withDate("Tue Aug 21 12:27:43 2018")
+                .withType(Packet.Type.TCP)
+                .withBytes(72)
+                .withSource("81-1-183-199.broadband.progtech.ru")
+                .withDestination("192.168.103.253")
+                .build();
+        String IPtoIP_PacketString = "Tue Aug 21 12:32:26 2018; UDP; eth0; 139 bytes; from 192.168.103.1:45323 to 255.255.255.255:5678\r";
+        String DNStoIP_PacketString = "Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to 192.168.103.253:ftp; first packet\r";
+        String DNStoDNS_PacketString = "Tue Aug 21 12:27:43 2018; TCP; eth0; 72 bytes; from 81-1-183-199.broadband.progtech.ru:57791 to www.yandex.ru:ftp; first packet\r";
+        Assertions.assertEquals(IPtoIP_Packet, parser.parseStep(IPtoIP_PacketString), "First parsed packet mismatch");
+        Assertions.assertEquals(IPtoDNS_Packet, parser.parseStep(DNStoIP_PacketString), "Second parsed packet mismatch");
+        Assertions.assertEquals(DNStoDNS_Packet, parser.parseStep(DNStoDNS_PacketString), "Third parsed packet mismatch");
     }
 }
